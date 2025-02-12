0850632ef48e7d14a80445cdab1c90ab42201492
RudiThoeni <r.thoeni@noi.bz.it>
2025-02-12 16:03:42 +0100

    using test api

--- src/api/config.js
+++ src/api/config.js
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 export default {	
-	API_BASE_URL: 'https://tourism.api.opendatahub.com/v1',
-	//API_BASE_URL: 'https://api.tourism.testingmachine.eu/v1',
+	//API_BASE_URL: 'https://tourism.api.opendatahub.com/v1',
+	API_BASE_URL: 'https://api.tourism.testingmachine.eu/v1',
 	//API_BASE_URL: 'https://localhost:5001/v1',
 	ORIGIN: 'webcomp-webcams'
 };
